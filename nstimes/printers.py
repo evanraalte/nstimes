fac65c361f11ad2d0486889c39b0747a88de313c
Erik van Raalte <erik.vanraalte@surf.nl>
2023-10-12 19:33:51 +0200

    black and sort

--- nstimes/printers.py
+++ nstimes/printers.py
@@ -1,9 +1,12 @@
 from typing import Protocol
 
-from nstimes.departure import Departure
-from rich.table import Table, Column
-from rich.console import Console
 from rich import print
+from rich.console import Console
+from rich.table import Column
+from rich.table import Table
+
+from nstimes.departure import Departure
+
 
 class Printer(Protocol):
     def generate_output(self):
@@ -15,15 +18,20 @@ class Printer(Protocol):
     def add_departure(self, departure: Departure):
         """adds a row to the departures"""
 
+
 def red(text):
     return f"[bold red]{text}[/bold red]"
+
+
 def cyan(text):
     return f"[bold cyan]{text}[/bold cyan]"
+
+
 def green(text):
     return f"[bold green]{text}[/bold green]"
 
 
-class ConsolePrinter():
+class ConsolePrinter:
     def __init__(self):
         self.buf = ""
         self.title = ""
@@ -37,20 +45,22 @@ class ConsolePrinter():
         self.title = title
 
     def add_departure(self, departure: Departure):
-        act_dep_time_str = departure.planned_departure_time.strftime('%H:%M')
-        delay_str = "" if departure.delay_minutes == 0 else red(f"+{departure.delay_minutes}")
+        act_dep_time_str = departure.planned_departure_time.strftime("%H:%M")
+        delay_str = (
+            "" if departure.delay_minutes == 0 else red(f"+{departure.delay_minutes}")
+        )
 
-        self.buf +=f"{departure.train_type:<3s} p.{departure.platform:>3s} in {departure.time_left_minutes:>2d} min ({act_dep_time_str}{delay_str})\n"
+        self.buf += f"{departure.train_type:<3s} p.{departure.platform:>3s} in {departure.time_left_minutes:>2d} min ({act_dep_time_str}{delay_str})\n"
 
 
-
-class ConsoleTablePrinter():
+class ConsoleTablePrinter:
     def __init__(self):
-        self.table = Table(Column("Train", justify="left"),
-                      Column("Platform", justify="right"),
-                      Column("Leaves in", justify="right"),
-                      Column("Departure time", justify="right"))
-
+        self.table = Table(
+            Column("Train", justify="left"),
+            Column("Platform", justify="right"),
+            Column("Leaves in", justify="right"),
+            Column("Departure time", justify="right"),
+        )
 
     def generate_output(self):
         Console().print(self.table)
@@ -59,10 +69,14 @@ class ConsoleTablePrinter():
         self.table.title = title
 
     def add_departure(self, departure: Departure):
-        act_dep_time_str = departure.planned_departure_time.strftime('%H:%M')
-        delay_str = "" if departure.delay_minutes == 0 else red(f"+{self.delay_minutes}")
+        act_dep_time_str = departure.planned_departure_time.strftime("%H:%M")
+        delay_str = (
+            "" if departure.delay_minutes == 0 else red(f"+{self.delay_minutes}")
+        )
 
-        self.table.add_row(departure.train_type,
-                           cyan(departure.platform),
-                           f"{cyan(departure.time_left_minutes)} min",
-                           f"{green(act_dep_time_str)}{delay_str}")
+        self.table.add_row(
+            departure.train_type,
+            cyan(departure.platform),
+            f"{cyan(departure.time_left_minutes)} min",
+            f"{green(act_dep_time_str)}{delay_str}",
+        )
