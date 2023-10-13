24b5d2f46f4336749f4eb8c10566d82b12909f13
Erik van Raalte <erik.vanraalte94@gmail.com>
2023-10-13 21:15:54 +0200

    fix bugs

--- nstimes/departure.py
+++ nstimes/departure.py
@@ -21,5 +21,8 @@ class Departure:
         return int(delay.total_seconds() / 60)
 
     def time_left_minutes(self, reference_time=datetime.now()) -> int:
+        reference_time = reference_time.replace(
+            tzinfo=self.actual_departure_time.tzinfo
+        )
         time_left = self.actual_departure_time - reference_time
         return int(time_left.total_seconds() / 60)
