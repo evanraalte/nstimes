2c61553313ef6b6ce61d455a45969005c899472e
Erik van Raalte <erik.vanraalte94@gmail.com>
2023-10-15 13:22:31 +0200

    add more type hints

--- tests/strategies.py
+++ tests/strategies.py
@@ -11,16 +11,18 @@ from nstimes.departure import Departure
 from nstimes.main import STATIONS_FILE
 
 # Define a Hypothesis strategy for generating times in the format "HH:MM"
-time_strategy = st.datetimes(
+time_strategy: st.SearchStrategy[datetime] = st.datetimes(
     min_value=datetime(2023, 1, 1),  # Specify an appropriate minimum date and time
     max_value=datetime(2023, 12, 31),  # Specify an appropriate maximum date and time
 )
 # Define a Hypothesis strategy for generating random delays in minutes
-delay_strategy = st.integers(min_value=0, max_value=120)  # Adjust the range as needed
+delay_strategy: st.SearchStrategy[datetime] = st.integers(
+    min_value=0, max_value=120
+)  # Adjust the range as needed
 
 
 @st.composite
-def departure_strategy(draw: st.DrawFn) -> Departure:
+def departure_strategy(draw: st.DrawFn) -> st.SearchStrategy[Departure]:
     planned = draw(time_strategy)
     time_difference_seconds = draw(st.integers(min_value=1, max_value=3600))
     actual = planned + timedelta(seconds=time_difference_seconds)
@@ -33,7 +35,9 @@ def departure_strategy(draw: st.DrawFn) -> Departure:
 
 
 @st.composite
-def two_different_stations_strategy(draw: st.DrawFn) -> tuple[str, str]:
+def two_different_stations_strategy(
+    draw: st.DrawFn,
+) -> st.SearchStrategy[tuple[str, str]]:
     with open(STATIONS_FILE, "r", encoding="utf-8") as file:
         uic_mapping = json.load(file)
     station_names = list(uic_mapping.keys())
