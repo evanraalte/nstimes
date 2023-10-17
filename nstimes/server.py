bf7f815f1081bb328746662bfddfade188bee5e3
Erik van Raalte <erik.vanraalte94@gmail.com>
2023-10-17 19:47:21 +0200

    add production check

--- nstimes/server.py
+++ nstimes/server.py
@@ -1,30 +1,71 @@
 import os
 from datetime import datetime
-from slowapi import Limiter, _rate_limit_exceeded_handler
 
 import uvicorn
+from dotenv import load_dotenv
 from fastapi import Depends
 from fastapi import FastAPI
 from fastapi import HTTPException
-from fastapi import status
 from fastapi import Request
+from fastapi import status
+from pydantic_settings import BaseSettings
+from slowapi import _rate_limit_exceeded_handler
+from slowapi import Limiter
+from slowapi.errors import RateLimitExceeded
+from slowapi.util import get_remote_address
+
 from nstimes.departure import Departure
 from nstimes.departure import get_departures
 from nstimes.utils import convert_to_rfc3339
 from nstimes.utils import get_uic_mapping
-from slowapi.util import get_remote_address
-from slowapi.errors import RateLimitExceeded
 
+
+class Settings(BaseSettings):  # type: ignore[misc]
+    virtual_host: str = ""
+    ns_api_token: str = ""
+
+def get_settings() -> Settings:
+    return Settings()
+
+load_dotenv()
 limiter = Limiter(key_func=get_remote_address)
-app = FastAPI()
-app.state.limiter = limiter
-app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+settings = Settings()
+
+
+def get_app(limiter: Limiter, settings: Settings) -> FastAPI:
+    settings = Settings()
+    print(settings)
+    if settings.virtual_host:
+        print("Rolling out production environment")
+        servers = [
+            {
+                "url": f"https://{settings.virtual_host}/",
+                "description": "Production environment",
+            }
+        ]
+        docs_url = None  # No docs for production environment
+    else:
+        print("Rolling out local environment")
+        servers = [
+            {"url": "http://localhost:8000/", "description": "Local environment"}
+        ]
+        docs_url = "/docs"
+
+    app = FastAPI(servers=servers, docs_url=docs_url)
+    app.state.limiter = limiter
+    app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+    return app
+
+
+app = get_app(limiter, settings)
+
 
 def get_token() -> str:
     try:
         return os.environ["NS_API_TOKEN"]
     except KeyError:
         raise HTTPException(status_code=500, detail=f"Could not find NS_API_TOKEN")
+
 
 @app.get("/stations")
 @limiter.limit(limit_value="5/minute")
