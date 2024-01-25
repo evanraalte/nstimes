90140e2a26a59431aa75f397e7bf7f00cf7acc15
Erik van Raalte <erik.vanraalte94@gmail.com>
2024-01-25 20:34:48 +0100

    Update actions.yml (#54)
    * Update actions.yml
    
    * f
    
    * f
    
    * f
    
    * f
    
    ---------
    
    Co-authored-by: Erik van Raalte <erik.vanraalte@surf.nl>

--- bump_version.py
+++ bump_version.py
@@ -1,4 +1,5 @@
 # mypy: ignore-errors
+import argparse
 import importlib.metadata
 import tomllib
 
@@ -10,13 +11,29 @@ def increment_patch_version(version: str) -> str:
     return f"{major}.{minor}.{patch + 1}"
 
 
+# Set up argument parser
+parser = argparse.ArgumentParser(
+    description="Increment patch version or set a specific version."
+)
+parser.add_argument("--version", type=str, help="Set a specific version (e.g., 1.2.3).")
+
+# Parse arguments
+args = parser.parse_args()
+if args.version:
+    if not all(x.isdigit() for x in args.version.split(".")):
+        print("Version must be in the form of x.y.z, where x, y, and z are integers.")
+        exit(1)
+
 try:
     with open("pyproject.toml", "rb") as f:
         data = tomllib.load(f)
         version = data["tool"]["poetry"]["version"]
-    data["tool"]["poetry"]["version"] = increment_patch_version(version)
+    data["tool"]["poetry"]["version"] = args.version or increment_patch_version(version)
     with open("pyproject.toml", "w") as f:
         toml.dump(data, f)
-    print(f"v{version}")
+    with open("pyproject.toml", "rb") as f:
+        data = tomllib.load(f)
+        version = data["tool"]["poetry"]["version"]
+        print(version)
 except importlib.metadata.PackageNotFoundError:
     print("Package not found.")
